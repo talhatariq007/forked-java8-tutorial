13cbcb904d7aa8489124f25be9cf92124ce5fd98
Talha Tariq <42859259+talhatariq007@users.noreply.github.com>
2018-09-03 11:05:24 +0500

    very important comment

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
-
+// one more change for pull :)
 /**
  * @author Benjamin Winterberg
  */
